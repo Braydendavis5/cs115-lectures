421b34532492089ffc91a73a2f8480c87404aa98
Braydendavis5 <bdavis5@nic.edu>
2025-03-03 00:09:31 +0000

    Finished most of the work

--- Davis_HW7.py
+++ Davis_HW7.py
@@ -1,3 +1,4 @@
+#This is to import pygame, so that I can make my game.
 import pygame
 
 #init pygame
@@ -16,80 +17,95 @@ clock = pygame.time.Clock()
 dt = 0
 speed = 10
 
-"""game loop"""
+#game loop
 running = True
 while running:
-  """Handle events"""
+  #Handle events
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
 
-  """update our game state"""
+  #update our game state
   
-  """draw to our screen"""
+  #draw to our screen
   #clear screen
   screen.fill("green")
 
-  #draw line
+  #draw sidewalk, at the bottum of the screen
   pygame.draw.line(
     screen, 
     "grey", 
     (0,600), (800,600), 50
   )
 
-    #draw line
+    #draw road1
   pygame.draw.line(
     screen, 
     "black", 
     (0,400), (800,400), 100
   )
 
-      #draw line
+      #draw road2
   pygame.draw.line(
     screen, 
     "black", 
     (0,200), (800,200), 100
   )
 
-      #draw line
+      #draw middle of road1
   pygame.draw.line(
     screen, 
     "yellow", 
     (0,400), (800,400), 5
   )
 
-        #draw line
+        #draw middle of road2
   pygame.draw.line(
     screen, 
     "yellow", 
     (0,200), (800,200), 5
   )
  
-  # draw rectangle
+  # draw square frog
   pygame.draw.rect(
     screen, 
     "dark green", 
     pygame.Rect((300,590),(25,25))
   )
 
+  # draw car1
+  pygame.draw.rect(
+    screen, 
+    "silver", 
+    pygame.Rect((500,360),(40,25))
+  )
 
+  # draw car2
+  pygame.draw.rect(
+    screen, 
+    "red", 
+    pygame.Rect((300,420),(40,25))
+  )
 
+  # draw car3
+  pygame.draw.rect(
+    screen, 
+    "white", 
+    pygame.Rect((400,220),(40,25))
+  )
 
-
-
-
-
-
-
-
+  # draw car4
+  pygame.draw.rect(
+    screen, 
+    "blue", 
+    pygame.Rect((220,160),(40,25))
+  )
 
   #update screen
   pygame.display.flip()
 
-
   #fps
   dt = clock.tick(speed)/1000
-
 
   #quit pygame
 pygame.quit()
